bb79d9e049c0135a708a8564da355124a8af13c3
Dickson Wong <dicksonwong@gmail.com>
2017-06-30 09:43:31 -0700

    Fix bot startup dependencies

--- run-bot.sh
+++ run-bot.sh
@@ -3,4 +3,11 @@
 cd "$(dirname "$0")"
 cd ./slackbot
 
+if [ ! -e "bot-access-token.secret" ]; then
+  >&2 echo "Error: Missing file 'slackbot/bot-access-token.secret'"
+  >&2 echo "Please create this file with the bot's access token obtained from Slack's app settings"
+  exit 1
+fi
+
+npm install
 exec npm start
